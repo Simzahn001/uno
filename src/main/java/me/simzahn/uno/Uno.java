3b78ac958006abefc88fad3b8fd5ecfd4d2f9895
Simzahn <simon.hochenberger@chello.at>
2022-05-31 22:52:20 +0200

    Initial Files

---
+++ src/main/java/me/simzahn/uno/Uno.java
@@ -0,0 +1,17 @@
+package me.simzahn.uno;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Uno extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
