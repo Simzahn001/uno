b1ad99c636fff111734ce25989792cf85718afd9
Simzahn001 <simon.hochenberger@gmail.com>
2025-08-01 13:34:11 +0200

    some other shit, 2

--- src/main/java/me/simzahn/uno/HopHop.go
+++ src/main/java/me/simzahn/uno/HopHop.go
@@ -1,9 +1,54 @@
-package main
+package hophop
 
 import (
 	"errors"
-	"fmt"
 )
+
+type element interface {
+	getID() string
+}
+
+// StaticSystem represents a static System. In contains all the information about the model and can be used to perform
+// calculations on it.
+type StaticSystem struct {
+	name  string
+	beams []Beam
+	nodes []Node
+}
+
+// NewStaticSystem creates a new, empty static system.
+func NewStaticSystem(name string) *StaticSystem {
+	return &StaticSystem{name, []Beam{}, []Node{}}
+}
+
+// Node represents the points of the static system. Other objects like beams or supports use them to define their
+// position and relation to other objects.
+type Node struct {
+	position Point
+}
+
+// Beam represents a static member, which is able be loaded with force.
+type Beam struct {
+	start, end Point
+	material   *Material
+	section    *Section
+}
+
+// Material represents a physical material, a beam is made of.
+type Material struct {
+	name           string
+	group          string
+	elasticModulus Value
+	strength       Value
+}
+
+// Section represents the geometrical definition of a beam.
+type Section struct {
+	area   Value
+	Iy, Iz Value
+	h, b   Value
+	ys, zs Value
+}
 
 type Value struct {
 	value            int
@@ -62,29 +107,8 @@ func (p Point) Equals(p2 Point) bool {
 	return p.x == p2.x && p.y == p2.y && p.z == p2.z
 }
 
-type Beam struct {
-	start, end Point
-	material   Material
-	section    Section
-}
-
-type Material struct {
-	name           string
-	group          string
-	elasticModulus Value
-	strength       Value
-}
-
-type Section struct {
-	area   Value
-	Iy, Iz Value
-	h, b   Value
-	ys, zs Value
-	geometry
-}
-
 // triangulation: ear clipping
-type Sketch2D struct {
+/*type Sketch2D struct {
 	sketchElements []Segment2D
 }
 
@@ -104,7 +128,7 @@ func (s Sketch2D) IsClosed() bool {
 // SegmentContainsPoint checks if any of the provided segments starts or ends at the specified point and returns a slice
 // without the segment, which contains the point, and the other point of this segment.
 // It returns an error if none of the segments contains the point.
-func SegmentContainsPoint(elements []Segment2D, point Point) ([]Segment2D, Point, error) {
+func segmentContainsPoint(elements []Segment2D, point Point) ([]Segment2D, Point, error) {
 	for i := 0; i < len(elements); i++ {
 		if point.Equals(elements[i].getEnd()) {
 			return append(elements[:i], elements[i+1:]...), elements[i].getStart(), nil
@@ -130,4 +154,4 @@ func (l Line) getStart() Point {
 
 func (l Line) getEnd() Point {
 	return l.end
-}
+}*/
